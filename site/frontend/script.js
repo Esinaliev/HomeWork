28d8a296be19940217942f2d44549d59e9842d35
ITstart <ITstart@new.local.itstep>
2021-01-11 18:00:37 +0600

    second

--- site/frontend/script.js
+++ site/frontend/script.js
@@ -1,13 +1,19 @@
 const url="https://reqres.in/api/users?page=2";
+const url1="http://localhost:3000/users";
 const vm=new Vue({
     el: "#app",
     data:{
         results: [],
-        msg:"Hello"
     },
     mounted(){
-        axios.get(url).then(res => {
-            this.results=res.data.data;
-        });
+        axios.get(url1).then(res => { this.results=res.data; });
+    },
+    methods:{
+        async deleteById(index) {
+            let id = this.results[index].id
+            this.results.splice(index,1)
+            //alert('deleted ' + id)
+            await axios.delete("http://localhost:3000/users/" + id)
+        }
     }
 });
