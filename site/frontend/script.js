6f9539d6141531e1e80b6a5951bb80d6708c8514
Arsen <76997450+Esinaliev@users.noreply.github.com>
2021-01-21 20:58:35 +0600

    put работает

--- site/frontend/script.js
+++ site/frontend/script.js
@@ -1,5 +1,8 @@
-const url1="https://reqres.in/api/users?page=2";
-const url="http://localhost:3000/users";
+const urlold1="https://reqres.in/api/users?page=2";
+const urlold2="http://localhost:3000/users";
+const url="http://first-appse.herokuapp.com/users";
+const urldaleka="http://daleka.herokuapp.com/users";
+const urlasi="http://firstmysait.herokuapp.com/users";
 
 const vm=new Vue({
     el: "#app",
@@ -21,17 +24,29 @@ const vm=new Vue({
             let id = this.results[index].id
             if(confirm('deleted ' + id)){
                 this.results.splice(index,1)
-                await axios.delete("http://localhost:3000/users/" + id)
+                await axios.delete(url+'/'+id)
             }
         },
-        async editById(index) {
-            alert('not work')
+        async editUser(index) {
+            let id = this.results[index].id
+            let newData = this.results[index]
+            if(this.password === null) alert("введите пароль")
+
+            newUser = {
+                first_name: newData.first_name,
+                last_name: newData.last_name,
+                password: newData.password,
+                avatar: newData.avatar,
+            };
+            await axios.put(url + "/" + id, newUser)
+            alert("User edited id:" + id);
         },
         async createNewUser() {
             if(this.newUser.first_name === null || this.newUser.last_name === null || this.newUser.password === null || this.newUser.avatar === null){
                 alert("заполните все данные")
             }
             else{
+                alert("новый пользователь создан")
                 console.log(this.user);
                 await axios.post(url,this.newUser).then(res => {
                     console.log(res.body);
@@ -39,10 +54,11 @@ const vm=new Vue({
                 
             }
         },
-        async getUserById(){
-            alert("work?")
+        async getUserById(index){
+            let id = this.results[index].id
+            this.results.splice(index,1)
+            await axios.get(url+'/'+id).then(res => newUser=res.data);
             //await axios.get("http://localhost:3000/users/"+ViewUsers.getElementById("indexid").value)
-            alert("work?")
         }
     }
 });
